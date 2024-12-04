c41c539f7c8131f642f9974bfaaf247e42cd5d44
david <dvlasic000@gmail.com>
2024-12-04 13:50:47 +0000

    Resolve conflict issues

---
+++ genre_detection.py
@@ -0,0 +1,9 @@
+import numpy as np
+import librosa
+import pickle
+
+# Load the pre-trained genre detection model
+MODEL_PATH = './genre_detection_model.pkl'
+
+with open(MODEL_PATH, 'rb') as model_file:
+    genre_model = pickle.load(model_file)
