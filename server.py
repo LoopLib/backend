905e48b5c071ec7e57b8e5131595e0577e4305ff
david <dvlasic000@gmail.com>
2025-02-16 15:30:06 +0000

    Resolve confilct

--- server.py
+++ server.py
@@ -86,4 +86,5 @@ def upload_file():
     return jsonify({'message': 'File uploaded successfully', 'bpm': bpm_final, 'key': key}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000, use_reloader=False)
+
