5fe01291a8f02221d48231f909c4047612271f3e
david <dvlasic000@gmail.com>
2025-04-21 13:33:50 +0100

    Change the port number for deployment

--- server.py
+++ server.py
@@ -135,4 +135,6 @@ def upload_file():
 
 # Run the Flask server when the script is executed directly
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000, use_reloader=False)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
